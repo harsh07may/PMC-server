9609ec603ba39a1acee3da821845d59f624aa235
Harsh Naik <harsh07may@gmail.com>
2023-04-20 13:53:37 +0530

    search audit pagination and inner-join fname

--- src/routes/digitizationRoute.ts
+++ src/routes/digitizationRoute.ts
@@ -129,24 +129,33 @@ router.get("/search", async (req, res) => {
 
 router.get(
   "/get-search-audit",
-  authMiddleware,
+
   async (req: Request, res: Response) => {
     try {
-      const userRole = req.User.userRoles;
-      const err = new AccessDeniedError("You need to be an Admin");
-      if (userRole != "admin") {
-        return res.status(err.statusCode).send({ error: err });
-      }
-
+      // const userRole = req.User.userRoles;
+      // const err = new AccessDeniedError("You need to be an Admin");
+      // if (userRole != "admin") {
+      //   return res.status(err.statusCode).send({ error: err });
+      // }
       const page = Number(req.query.page) || 1;
       const limit = 10;
       const offset = (page - 1) * limit;
+      const count = await pool.query(
+        "SELECT count(*) from searchadd_auditlogs"
+      );
+      if (offset > count.rows[0].count) {
+        return res.status(404).send("Records not found");
+      }
       const document = await pool.query(
-        `SELECT * FROM searchadd_auditlogs ORDER BY id DESC LIMIT ${limit} OFFSET ${offset}`
+        "SELECT a.*, u.fullname FROM searchadd_auditlogs a INNER JOIN users u ON a.performedby = u.username ORDER BY logid DESC LIMIT $1 OFFSET $2",
+        [limit, offset]
       );
 
       if (document.rowCount === 0) throw new Error("Audit not found");
-      res.send(document.rows);
+      res.json({
+        rows: document.rows,
+        total: count.rows[0].count,
+      });
     } catch (error) {}
   }
 );
