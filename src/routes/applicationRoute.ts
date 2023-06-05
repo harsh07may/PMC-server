0962a35b4667d42f1db256cd3fce29d53d4995c7
Harsh Naik <harsh07may@gmail.com>
2023-06-05 10:38:19 +0530

    updated transferApplication and getPending

--- src/routes/applicationRoute.ts
+++ src/routes/applicationRoute.ts
@@ -2,14 +2,13 @@ import { Router, Response, Request } from "express";
 export const router = Router();
 import {
   addNewApplication,
+  checkTrail,
   fetchApplication,
+  fetchTrailByStatus,
+  transferApplication,
 } from "../services/applicationService";
 
-import {
-  BadRequestError,
-  ExistingUserError,
-  ResourceNotFoundError,
-} from "../models/errors";
+import { BadRequestError, ResourceNotFoundError } from "../models/errors";
 import { logger } from "../utils/logger";
 import { authMiddleware } from "../authMiddleware";
 
@@ -26,6 +25,19 @@ router.get("/getall", async (req: Request, res: Response) => {
   }
 });
 
+router.get("/getPending", async (req: Request, res: Response) => {
+  try {
+    const { dept } = req.query;
+    const Department = dept ? String(dept) : "";
+
+    const pending = await fetchTrailByStatus(Department, "unseen");
+    res.send(pending.rows);
+  } catch (err: any) {
+    logger.log("error", err);
+    res.send(err);
+  }
+});
+
 router.post("/createApplication", async (req: Request, res: Response) => {
   try {
     const { ref_id, title } = req.body;
@@ -33,19 +45,43 @@ router.post("/createApplication", async (req: Request, res: Response) => {
     if (application.rows.length > 0) {
       logger.log("error", `Application with reference No. already exists.`);
       throw new BadRequestError(
-        "Application with reference no. already exists."
+        `Application with reference no.  ${ref_id} already exists.`
       );
     }
     await addNewApplication(ref_id, title);
-    res
-      .status(200)
-      .send(`Successfully created Application with Reference No. ${ref_id}`);
+    res.send(`Successfully created Application with Reference No. ${ref_id}`);
   } catch (err: any) {
     res.send(err);
   }
 });
 
 router.post("/transferApplication", async (req: Request, res: Response) => {
+  try {
+    const { ref_id, reciever } = req.body;
+    const application = await fetchApplication(ref_id);
+    if (application.rows.length == 0) {
+      logger.log(
+        "error",
+        `Application with reference No. ${ref_id} does not already exist.`
+      );
+      throw new BadRequestError(
+        `Application with reference no. ${ref_id} does not exist.`
+      );
+    }
+    const transfer_no = await checkTrail(ref_id);
+    await transferApplication(
+      ref_id,
+      transfer_no,
+      application.rows[0].holder,
+      reciever
+    );
+    res.send(`Application ${ref_id} transferred to ${reciever}`);
+  } catch (err: any) {
+    res.send(err);
+  }
+});
+
+router.post("/updateStatus", async (req: Request, res: Response) => {
   try {
   } catch (err: any) {
     // Handle error here
